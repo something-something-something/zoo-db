cb6e9574d568b800c5dd23a3b6f0ba452751fc79
Eric Krebs <ekrebs@outlook.com>
2017-03-28 01:53:19 -0500

    added a redirect

--- src/includes/aftersetup.php
+++ src/includes/aftersetup.php
@@ -2,6 +2,7 @@
 /*for files that include this it ensure they are not accesed before setup is complete.*/
 /* if db.json exists the database was probably already setup*/
 if(!file_exists('settings/files/db.json')){
+	header('Location: /setupform.php');
 	die();
 }
 ?>
