500eace70b84b1e6509a1a9825597548c77a070f
Eric Krebs <ekrebs@outlook.com>
2017-04-01 17:31:31 -0500

    added animal creation form

--- src/employee/index.php
+++ src/employee/index.php
@@ -10,4 +10,5 @@
 ?>
 you are logged in now. See <a href="employeelist.php">the employee list</a> if you are a superUser<br>
 See <a href="createuserform.php">Create a User</a> if you are a superUser<br>
-See <a href="createdepartmentform.php">Create a Department</a> if you are a superUser
+See <a href="createdepartmentform.php">Create a Department</a> if you are a superUser<br>
+See <a href="createanimalform.php">Create a Animal</a> if you are a superUser
