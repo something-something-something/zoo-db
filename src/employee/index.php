71e27989824c1cb32f90e8aff0771dbce90da854
Eric Krebs <ekrebs@outlook.com>
2017-04-04 15:30:36 -0500

    added ability to add habitats changed Habitats table

--- src/employee/index.php
+++ src/employee/index.php
@@ -12,5 +12,6 @@ you are logged in now. See <a href="employeelist.php">the employee list</a> if y
 See <a href="createuserform.php">Create a User</a> if you are a superUser<br>
 See <a href="createdepartmentform.php">Create a Department</a> if you are a superUser<br>
 See <a href="createanimalform.php">Create a Animal</a> if you are a superUser<br>
+See <a href="createhabform.php">Create a Habitat</a> if you are a superUser<br>
 See <a href="animallist.php">the animal list</a> if you are a superUser<br>
 See <a href="deptlist.php">the Department list</a> if you are a superUser<br>
