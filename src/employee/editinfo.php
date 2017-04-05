b81184333dc1acd9fb2720206821e199e42b1357
Eric Krebs <ekrebs@outlook.com>
2017-04-04 20:03:00 -0500

    small fixes

--- src/employee/editinfo.php
+++ src/employee/editinfo.php
@@ -4,10 +4,11 @@
 	require_once('../includes/mysqlcon.php');
 	require_once('../func/empluser.php');
 	require_once('../includes/checkcsrf.php');
+	EmplUser\restrictPageToLoggedIn();
 ?>
 <?php
 //TODO validate Posts
-$statment=$db->prepare("update employee set employeeemail=(?), address=(?) where employeeID=?");
+$statment=$db->prepare("update Employee set employeeEmail=(?), address=(?) where employeeID=?");
 echo $db->error;
 $statment->bind_param('ssi',$_POST['email'],$_POST['address'],$_SESSION['EMPLID']);
 
--- src/employee/editinfoform.php
+++ src/employee/editinfoform.php
@@ -1,15 +1,26 @@
-<h2> Edit Your Information </h2>
 <?php
 	require_once('../includes/initses.php');
 	require_once('../includes/aftersetup.php');
 	require_once('../includes/mysqlcon.php');
 	require_once('../func/empluser.php');
 	require_once('../func/dept.php');
+	EmplUser\restrictPageToLoggedIn();
+?>
+<h2> Edit Your Information </h2>
+<?php
+$statment=$db->prepare("select employeeEmail, address from Employee where employeeID=?");
+$statment->bind_param('i',$_SESSION['EMPLID']);
+//echo $db->error;
+$statment->execute();
+$statment->bind_result($email, $address);
+if(!$statment->fetch()){
+	die('can\'t acces your info');
+}
+
 ?>
 <form action="editinfo.php" method="POST">
-	Email: <input type="text" value="" name="email"><br>
-	Address: <input type="text" name="address"><br>
-	<?php echo Dept\selectDeptHTML($db); ?><br>
+	Email: <input type="text" value="<?php echo $email ?>" name="email"><br>
+	Address: <input type="text" value="<?php echo $address ?>" name="address"><br>
 	<input type="hidden" value="<?php echo($_SESSION['CSRF']);?>" name="csrf">
 	<input type="submit">
 </form>
