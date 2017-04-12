59289efa504bd6fcc2f377e661e1ccf2f7ea0e98
Eric Krebs <ekrebs@outlook.com>
2017-04-11 20:58:39 -0500

    added styling to tickets

--- src/employee/tickets.php
+++ src/employee/tickets.php
@@ -1,10 +1,12 @@
 <?php
 	require_once('../includes/initses.php');
 	require_once('../includes/aftersetup.php');
+	require_once('../func/fancy.php');
 	require_once('../includes/mysqlcon.php');
 	require_once('../func/empluser.php');
 	EmplUser\restrictPageToPositions($db,["superUser","ticketSeller"]);
 ?>
+<?php Fancy\printHeader($db,'Tickets','employee'); ?>
 <form action="sellticket.php" method="POST">
 	<select required name="type">
 		<option value="adult">adult $10</option>
@@ -20,15 +22,20 @@
 	$statment=$db->prepare("select serialnumber,tickettype,ticketprice,date from Tickets");
 	$statment->execute();
 	$statment->bind_result($num,$type,$price,$date);
+	echo '<table><thead><tr><th>Ticket Number</th><th>Type</th><th>Price</th><th>Date</th><th>Delete</th></thead><tbody>';
 	while($statment->fetch()){
-		echo 'ticket#'.$num.' '.$type.' '.$price.' '.$date;
+		echo '<tr>';
+		echo '<td>'.$num.'</td><td>'.$type.'</td><td>'.$price.'<td>'.$date.'</td>';
 		echo <<<DELETETICKET
-		<form action="deleteticket.php" method="POST">
+		<td><form action="deleteticket.php" method="POST">
 			<input type="hidden" name="num" value="$num">
 			<input type="hidden" value="{$_SESSION['CSRF']}" name="csrf">
-		Delete<input type="submit">
-		</form>
+			<input value="Delete" type="submit">
+		</form></td>
 DELETETICKET;
+		echo '</tr>';
 	}
+	echo '</tbody></table>';
 	$statment->close();
 ?>
+<?php Fancy\printFooter(); ?>
