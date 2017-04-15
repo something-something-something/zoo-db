8bd75aebe3376710098706991406b288cf895f07
Eric Krebs <ekrebs@outlook.com>
2017-04-15 12:07:54 -0500

    from ron fixes to animal files

--- src/employee/createanimalform.php
+++ src/employee/createanimalform.php
@@ -4,18 +4,21 @@
 	require_once('../includes/mysqlcon.php');
 	require_once('../func/empluser.php');
 	require_once('../func/dept.php');
+	require_once('../func/hab.php');
 	require_once('../func/fancy.php');
 	EmplUser\restrictPageToPositions($db,['superUser']);
 ?>
 <?php Fancy\printHeader($db,'Create an Animal','employee','animal'); ?>
 <form action="createanimal.php" method="POST">
-	name: <input type="text" value="" name="name"><br>
-	species: <input type="text" name="tax"><br>
-	dob: <input type="date" value="2010-01-19" name="dob"><br>
+	Name: <input type="text" value="" name="name" required><br>
+	Species: <input type="text" name="tax" required><br>
+	DOB/Date Arrived: <input type="date" value="2010-01-19" name="dob"><br>
 	Male<input type="radio" name="sex" value="m">
 	Female<input type="radio" checked name="sex" value="f"><br>
-	<?php echo Dept\selectDeptHTML($db); ?><br>
+	Department: <?php echo Dept\selectDeptHTML($db); ?><br>
+	Enclosure: <?php echo Habitat\selectHabitatHTML($db); ?><br>
 	<input type="hidden" value="<?php echo($_SESSION['CSRF']);?>" name="csrf">
+
 	<input type="submit">
 </form>
 <?php Fancy\printFooter(); ?>
