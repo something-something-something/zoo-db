efc05f04fa561561a3767d1e7c64af7fa5fdac3b
Eric Krebs <ekrebs@outlook.com>
2017-04-15 18:11:39 -0500

    added vendor selection menus

--- src/employee/deptgrossvendorsalesform.php
+++ src/employee/deptgrossvendorsalesform.php
@@ -4,16 +4,17 @@
 	require_once('../includes/mysqlcon.php');
 	require_once('../func/empluser.php');
 	require_once('../func/dept.php');
+	require_once('../func/vend.php');
 	EmplUser\restrictPageToPositions($db,['superUser','departmentManager','vendor']);
 	require_once('../func/fancy.php');
 ?>
 <?php Fancy\printHeader($db,'Vendor Salses','employee','vendor'); ?>
 <form action="deptgrossvendorsales.php" method="POST">
 	<h1>Enter Sales Amount</h1>
-	Vendor ID: <input type="number" name="id"><br>
+	Vendor: <?php echo Vendor\selectVendorInEmplDeptHTML($db,$_SESSION['EMPLID']); ?>
+	<br>
 	Day: <input type="date" name="day"><br>
 	Sales Amount: <input type="number" value="" name="saleamount"><br>
-	<?php echo Dept\selectDeptHTML($db); ?><br>
 	<input type="hidden" value="<?php echo($_SESSION['CSRF']);?>" name="csrf"> <br>
 	<input type="submit">
 </form>
