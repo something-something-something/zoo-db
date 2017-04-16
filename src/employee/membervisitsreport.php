a5d345a89acca851c24fca1623513955a2f4b043
Eric Krebs <ekrebs@outlook.com>
2017-04-15 20:45:58 -0500

    fix from ron

--- src/employee/membervisitsreport.php
+++ src/employee/membervisitsreport.php
@@ -16,14 +16,24 @@ $statment->bind_param('ss',$_POST['startDate'], $_POST['endDate']);
 $statment->execute();
 $statment->bind_result($fname, $lname, $memID, $visitDate, $numofpeople);
 $sum = 0;
+echo "<table width=\"50%\">
+  <tr>
+    <th>First Name</th>
+    <th>Last Name</th> 
+    <th>Member ID</th>
+    <th>Visit Date</th>
+    <th>Number of People</th>
+  </tr>";
 while($statment->fetch()){
 		//echo $numofpeople . ' ';
 		$sum += $numofpeople;
-		echo 'First Name: '.$fname.' ';
-		echo 'Last Name: '.$lname.' ';
-		echo 'Member ID: '.$memID.' ';
-		echo 'Date: '.$visitDate.' <br>';
+		echo '<tr><td align="center">'.$fname.'</td>';
+		echo '<td align="center">'.$lname.'</td>';
+		echo '<td align="center">'.$memID.'</td>';
+		echo '<td align="center">'.$visitDate.'</td>';
+		echo '<td align="center">'.$numofpeople.'</td></tr>';
 }
+echo '</table><br><hr>';
 echo "<i>Total number of people visited including guests: " . $sum . '</i>';
 $statment->close();
 	
